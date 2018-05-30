7ee81d472f3878e04266a97d8f3702e305565885
Fabian Gülzau <guelzauf@sowi.hu-berlin.de>
2018-05-30 11:40:43 +0200

    Included color plotting

--- analysis/BorderLines.R
+++ analysis/BorderLines.R
@@ -20,11 +20,27 @@ borders <- gDifference(
   as(gUnaryUnion(europe.sp),"SpatialLines"),
   byid=TRUE)
 
+## Test case for color plotting of borders ## 
+
+# Create an identifying variable without missings
+iso_a3 <- europe.sp@data$iso_a3 
+iso_a3[[21]] <- "XKX"
+
+# Add the variable to the SpatialLines object
+for (id in 1:38) {
+  borders@lines[[id]]@ID <- iso_a3[[id]]
+}
+
+# Create a SpatialLinesDataFrame
+borders.spdf <- SpatialLinesDataFrame(borders, data = data.frame(iso_a3 = iso_a3,
+                                                                variable = round(seq(1, 4, length.out = 38), 0),
+                                                                row.names = iso_a3),
+                                     match.ID = TRUE)
+
 # Plotting
 # Coloring could be adjusted following: https://gis.stackexchange.com/questions/91775/coloring-spatiallines-by-attribute-in-r
 # Base map
 plot(europe.sp)
 
 # Overplot with emphasized (shared) borders
-plot(borders, add = TRUE, lwd = 2, col = "navy") 
-
+plot(borders.spdf, add = TRUE, lwd = 2, col = borders.spdf@data$variable) 
