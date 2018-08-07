c38f1c04b6bece995fe3e55aafe36cd78fabcaf5
FabianFox <fabianguelzau@web.de>
2018-08-07 13:51:46 +0200

    Added plot

--- analysis/MigratoryRoutesDataFrontex.R
+++ analysis/MigratoryRoutesDataFrontex.R
@@ -3,7 +3,7 @@
 
 # Load/install packages
 if (!require("pacman")) install.packages("pacman")
-p_load(tidyverse, rio)
+p_load(tidyverse, rio, sf, rnaturalearth)
 
 # from Frontex (xlsx)
 ## ------------------------------------------------------------------------------------------------------------ ##
@@ -12,3 +12,13 @@ routes.df <- import(file = "https://frontex.europa.eu/assets/Migratory_routes/De
   gather(year, crossings, -c(Route, BorderLocation, NationalityLong)) %>%
   mutate(year = as.Date(as.numeric(year), origin = "1899-12-30"))
 
+# Base map from rnaturalearth
+world.sf <- ne_countries(returnclass = "sf") %>%
+  filter(region_wb %in% c("Europe & Central Asia", "Middle East & North Africa")) 
+
+# Plot
+ggplot(data = world.sf) +
+  geom_sf() +
+  coord_sf(xlim = c(-20, 50), ylim = c(30, 65)) +
+  geom_bspline(data = lines.df, mapping =  aes(x = lon, y = lat, group = route),
+                                               arrow = arrow(length = unit(0.2, unit = "cm")))
