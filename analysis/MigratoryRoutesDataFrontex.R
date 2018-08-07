5d1e427a1a41d347cfe398e8bc971ba0bdc7fd53
FabianFox <fabianguelzau@web.de>
2018-08-07 13:53:51 +0200

    Added geocode for lines

--- analysis/MigratoryRoutesDataFrontex.R
+++ analysis/MigratoryRoutesDataFrontex.R
@@ -3,7 +3,7 @@
 
 # Load/install packages
 if (!require("pacman")) install.packages("pacman")
-p_load(tidyverse, rio, sf, rnaturalearth)
+p_load(tidyverse, rio, sf, rnaturalearth, ggmap)
 
 # from Frontex (xlsx)
 ## ------------------------------------------------------------------------------------------------------------ ##
@@ -15,6 +15,31 @@ routes.df <- import(file = "https://frontex.europa.eu/assets/Migratory_routes/De
 # Base map from rnaturalearth
 world.sf <- ne_countries(returnclass = "sf") %>%
   filter(region_wb %in% c("Europe & Central Asia", "Middle East & North Africa")) 
+
+# Create a data set for the lines
+# (1) Data set with empty coordinates which roughly comply with the routes provided by Frontex
+lines.df <- tibble(
+  route = c(rep("Black Sea", 2),
+            rep("Central Mediterranean", 3)),
+  location = c("Amasra, Turkey", "Vama Veche, Romania", 
+               "Sirte, Libya", "Birbuba, Malta", "Caltanissetta, Italy"),
+  lon = NA,
+  lat = NA
+)
+
+# (2) Fill the missing coordinates using ggmap::geocode
+# for loop with if-condition for warnings
+# run several times until no more NA's are returned
+for(i in 1:nrow(lines.df)) {
+  if (is.na(lines.df$lon[i])) {
+    result <- tryCatch(geocode(lines.df$location[i], output = "latlon", source = "google"),
+                       warning = function(x) data.frame(lon = NA, lat = NA))
+    lines.df$lon[i] <- as.numeric(result[1])
+    lines.df$lat[i] <- as.numeric(result[2])
+  }
+  i <- i + 1
+  Sys.sleep(sample(seq(.5, 2, 0.5), 1))  
+}
 
 # Plot
 ggplot(data = world.sf) +
