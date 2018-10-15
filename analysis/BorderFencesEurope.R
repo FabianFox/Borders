4fba6f21f9548d0570be273473bc5ee429105cf7
FabianFox <fabianguelzau@web.de>
2018-10-15 16:40:28 +0200

    updated

--- analysis/BorderFencesEurope.R
+++ analysis/BorderFencesEurope.R
@@ -17,6 +17,15 @@
 # New York Times (16.10.2015)
 # Link: https://www.nytimes.com/interactive/2015/09/15/world/europe/migrant-borders-europe.html
 
+# Reuters (27.04.2016) Austria only
+# Link: https://www.reuters.com/article/us-europe-migrants-brenner-idUSKCN0XO1HS
+
+# DW (18.09.2015) HUN HRV
+# Link: https://www.dw.com/en/hungary-starts-building-fence-on-croatian-border/a-18721670
+
+# Baltic Times (05.02.2017) LVA RUS
+# Link: https://www.baltictimes.com/fence_stretching_23-km_already_up_on_latvia-russia_border/
+
 # Load/install packages
 if (!require("pacman")) install.packages("pacman")
 p_load(tidyverse)
