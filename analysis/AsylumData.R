bafd935937b22b1ca78dc5fdbe749ae92c0b7ed8
FabianFox <fabianguelzau@web.de>
2018-11-20 18:08:25 +0100

    Code readability

--- analysis/AsylumData.R
+++ analysis/AsylumData.R
@@ -1,12 +1,13 @@
 # Asylum Data
 
 # Load/install packages
-### ------------------------------------------------------------------------------------------------------------------ ###
+### ------------------------------------------------------------------------###
 
 if (!require("pacman")) install.packages("pacman")
-p_load(tidyverse, rio, rvest, janitor, qdap, eurostat, countrycode, wbstats, lubridate, countrycode)
+p_load(tidyverse, rio, rvest, janitor, qdap, eurostat, countrycode, wbstats, 
+       lubridate, countrycode)
 
-### ------------------------------------------------------------------------------------------------------------------ ###
+### ------------------------------------------------------------------------###
 
 # Border Management and Schengen
 borderURL <- "https://ec.europa.eu/home-affairs/what-is-new/eu-law-and-monitoring/infringements_en?country=All&field_infringement_policy_tid=1625&field_infringement_number_title="
@@ -29,7 +30,8 @@ urls <- list(borderURL, asylumURL, returnURL)
 
 # Add additional data:
 # - number of asylum applications (eurostat)
-# ID: tps00191: Asylum and first time asylum applicants - annual aggregated data (rounded)
+# ID: tps00191: Asylum and first time asylum applicants - annual aggregated data 
+#     (rounded)
 # Metadata: http://ec.europa.eu/eurostat/cache/metadata/en/migr_asyapp_esms.htm
 # Further information: https://bit.ly/2LXrsKB and http://dd.eionet.europa.eu/vocabulary/eurostat/asyl_app
 
@@ -37,8 +39,10 @@ urls <- list(borderURL, asylumURL, returnURL)
 # clean_eurostat_cache(cache_dir = NULL)
 
 # Yearly applications from 2015 - 2017
-asylum.df <- get_eurostat("tps00191", time_format = "num", stringsAsFactors = FALSE,
-                       filters = list(asyl_app = "ASY_APP", time = 2015:2017)) %>%
+asylum.df <- get_eurostat("tps00191", time_format = "num", 
+                          stringsAsFactors = FALSE,
+                          filters = list(asyl_app = "ASY_APP", 
+                                      time = 2015:2017)) %>%
   select(country = geo, applicants = values, year = time) %>%
   filter(country != "EU28") %>%
   mutate(country = countrycode(country, "eurostat", "iso3c")) 
@@ -79,7 +83,7 @@ wb.info <- wb(country = asylum.df$country,
 # (2) Match to base data
 asylum.df <- asylum.df %>%
   left_join(wb.info, by = c("country" = "iso3c", "year")) %>%
-  mutate(applicants_perc = applicants / (population / 1000)) 
+  mutate(applicants_per1000 = round(applicants / (population / 1000), 2)) 
 
 # Scrape over urls and combine df
 infringement.df <- map(urls, ext_table) %>%
@@ -91,7 +95,8 @@ infringement.df <- map(urls, ext_table) %>%
   mutate(country = countrycode(country, "country.name.en", "iso3c"),
          year = as.numeric(str_extract(decision_date, "(?<=/)[:digit:]{4}")))
 
-# Infringment cleaned for join (policy field is disregarded here, i.e. doesn't matter whether infringement is due to 
+# Infringment cleaned for join (policy field is disregarded here, 
+# i.e. doesn't matter whether infringement is due to 
 # Border Management, Asylum or Return Policies)
 infringement.join <- infringement.df %>%
   select(country, year) %>%
@@ -106,7 +111,8 @@ asylum.df <- asylum.df %>%
 # ParlGov data (http://www.parlgov.org/, accessed: 15.11.2018)
 election.df <- import("./FRAN-reports/view_election.csv") %>%
   mutate(election_date = as_date(election_date)) %>%
-  filter(between(year(election_date), 2010, 2017), country_name_short %in% unique(asylum.df$country),
+  filter(between(year(election_date), 2010, 2017), 
+         country_name_short %in% unique(asylum.df$country),
          election_type == "parliament") %>%
   group_by(country_name_short, election_date) %>%
   summarize(right_share = sum(vote_share[left_right > 8], na.rm = T)) %>%
@@ -119,7 +125,8 @@ election.df <- import("./FRAN-reports/view_election.csv") %>%
 # (1) Create a "full"-DB with all possible combinations of country-years;
 #     Fill missing values with earlier occurrence
 asyl_election_full.df <- asylum.df %>%
-  full_join(election.df, by = c("country" = "country_name_short", "year" = "round_year")) %>%
+  full_join(election.df, 
+            by = c("country" = "country_name_short", "year" = "round_year")) %>%
   arrange(country, year) %>%
   group_by(country) %>%
   fill(right_share, .direction = "down") %>%
@@ -127,10 +134,39 @@ asyl_election_full.df <- asylum.df %>%
 
 # (2) Join right_share to asylum.df
 asylum.df <- asylum.df %>%
-  left_join(asyl_election_full.df, by = c("country", "year"))
+  left_join(asyl_election_full.df, by = c("country", "year")) %>%
+  mutate(region = countrycode(country, "iso3c", "region"),
+         EU28 = countrycode(country, "iso3c", "eu28")) %>%
+  filter(EU28 == "EU")
+
+# Tables
+### ------------------------------------------------------------------------###
+
+# Table 1. Country, application_per1000, region in 2015
+table1 <- asylum.df %>%
+  filter(year == 2015, EU28 == "EU") %>%
+  select(country, region, applicants_per1000) %>%
+  mutate(group = case_when(
+    region == "Eastern Europe" ~ "Visegrad and Eastern EU",
+    region %in% c("Northern Europe", "Western Europe") & 
+      applicants_per1000 > 2 ~ "Host states",
+    country %in% c("MLT", "CYP", "ITA", "GRC", "ESP") ~ "Frontline states",
+    TRUE ~ "Other"
+  )) %>%
+  arrange(factor(group, levels = c("Visegrad and Eastern EU", "Host states", 
+                                   "Frontline states", "Other")),
+          region,
+          country,
+          desc(applicants_per1000))
+
+# To word
+write.table(table1, file = "./output/table1.txt", sep = ",", quote = FALSE, 
+            row.names = F)
+
+
 
 # Exploratory visualizations of the data
-### ------------------------------------------------------------------------------------------------------------------ ###
+### ------------------------------------------------------------------------###
 
 # Visualizations using the "full" infringement data set
 # Grouped by country/year - individual plots by policy
@@ -146,7 +182,7 @@ inf.count <- infringement.df %>%
                        ylab("") +
                        xlab("") +
                        scale_x_continuous(breaks = seq(2005, 2015, 5)) +
-                       scale_y_continuous(labels = function(x) round(x, digits = 0)) +
+                       scale_y_continuous(labels = function(x) round(x, 0)) +
                        theme_minimal() +
                        theme(panel.grid.minor.x = element_blank(),
                              panel.grid.major.x = element_blank(),
@@ -186,8 +222,9 @@ recognition.fig <- asylum.df %>%
         axis.ticks.x = element_line(size = .5))
 
 # Saving data and figures
-### ------------------------------------------------------------------------------------------------------------------ ###
+### ------------------------------------------------------------------------###
 # Data:
 saveRDS(asylum.df, file = "./data/AsylumStatistics.rds")
 # Plots:
-ggsave(filename = "./FRAN-reports/AsylumInfringmentProceduresFig.tiff", plot = inf.count$plot[[1]], device = "tiff", dpi = 600)
+ggsave(filename = "./FRAN-reports/AsylumInfringmentProceduresFig.tiff", 
+       plot = inf.count$plot[[1]], device = "tiff", dpi = 600)
