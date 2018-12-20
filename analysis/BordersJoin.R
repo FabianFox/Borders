ad9e9a8bb0521572b777637e40d6d800a795a54a
FabianFox <fabianguelzau@web.de>
2018-12-20 16:10:48 +0100

    bind datasets

--- analysis/BordersJoin.R
+++ analysis/BordersJoin.R
@@ -1,29 +1,47 @@
 # Join existing data on border barriers to our source data
-# 
+### ------------------------------------------------------------------------ ###
+
 # Data from: 
 # barriers.bw - Jones, Reece (2012) Border Walls: Security and the War on Terror, London: Zed Books, p. 10.
 # barriers.wiki - https://en.wikipedia.org/wiki/Border_barrier
-# barriers.hw - https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/U16NEI
+# barriers.hw - Hassner & Wittenberg (2015) https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/U16NEI
+# barriers.jg . Jellissen & Gottheil (2013) On the utility of security fences along international borders
 
-# Source data created in "SourceData.R"
+# Load/install packages
+### ------------------------------------------------------------------------ ###
+if(!require("pacman")) install.packages("pacman")
+p_load(tidyverse, pryr)
+
+### Source data created in "SourceData.R" ###
+
+# Load existing data sets
+### ------------------------------------------------------------------------ ###
 
 # Select variables to keep for the join
 vars <- c("state1", "state2", "year", "indicator", "source")
 
+# Reece (2012)
+# generated in: Jones2012 - BorderWalls_Data.R
 barriers.bw.join <- barriers.bw %>%
   select(vars) %>%
   mutate(year = as.character(year)) %>%
-  rename(built = year)
+  rename(year = built)
 
+# Wikipedia: Border Barrier
+# generated in: BorderBarriers_Wiki.R
 barriers.wiki.join <- barriers.wiki %>%
   select(vars) %>%
-  rename(built = year)
+  rename(year = built)
+
+# Hassner & Wittenberg (2015)
+# generated in: Hassner&Wittenberg2015 - BarriersToEntry_Data.R
+#  
+
+# Jellissen & Gottheil (2013)
+# generated in: Jellissen&Gottheil2013 - OnTheUtilityOfSecurityFencesAlongInternationalBorders.R
+barriers.jg.join <- barriers.jg %>%
+  select(vars) # Check year variable
 
 # Join
-# barrier.bw
-border.df <- border.df %>%
-  left_join(barriers.bw.join, by = c("state1", "state2"))
-
-# barriers.wiki
-border.df <- border.df %>%
-  left_join(barriers.wiki.join, by = c("state1", "state2"), suffix = c(".bw", ".wiki"))
+### ------------------------------------------------------------------------ ###
+barriers.df <- bind_rows(barriers.bw.join, barriers.jg.join, barriers.wiki.join)
