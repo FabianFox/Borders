54d439e4cf7184746515b1da6e141601acc49dd7
FabianFox <fabianguelzau@web.de>
2019-02-12 11:31:42 +0100

    Tables from Avdan (2019) in R

---
+++ analysis/Fence data/Avdan 2019 - VisasAndWall.R
@@ -0,0 +1,38 @@
+# Data from Avdan 2019, p. 128-129
+# 
+# Data from: 
+# Avdan, Nazli (2019): Visas and Walls: Border Security in the Age of Terrorism,
+# Philadelphia: University of Pennsylvania Press.
+
+# Load/install packages
+### ------------------------------------------------------------------------ ###
+if (!require("pacman")) install.packages("pacman")
+p_load(tidyverse, countrycode, rio, tabulizer)
+
+# Extract data from pdf table
+### ------------------------------------------------------------------------ ###
+# Page 128
+barriers.av.t1 <- import("./data/border data/Avdan 2019 - Visas and Walls_1.xlsx", range = "A3:D44") %>%
+  .[-36,] %>%
+  set_names(c("state1", "state2", "year", "dismantled")) %>%
+  mutate(state1 = countrycode(state1, "country.name.en", "iso3c"),
+         state2 = countrycode(state2, "country.name.en", "iso3c"),
+         indicator = "fortified",
+         source = "Avdan (2019)") %>%
+  filter(is.na(dismantled))
+
+# Page 130
+barriers.av.t2 <- import("./data/border data/Avdan 2019 - Visas and Walls_2.xlsx", range = "A2:D41") %>%
+  .[-c(27, 35),] %>%
+  set_names(c("state1", "state2", "year", "dismantled")) %>%
+  mutate(state1 = countrycode(state1, "country.name.en", "iso3c"),
+         state2 = countrycode(state2, "country.name.en", "iso3c"),
+         indicator = "fortified",
+         source = "Avdan (2019)") %>%
+  filter(is.na(dismantled))
+
+# (2) Combine
+barriers.av <- bind_rows(barriers.av.t1, barriers.av.t2)
+
+# (4) Save data
+saveRDS(barriers.av, file = "./data/border data/Avdan 2019.rds")
