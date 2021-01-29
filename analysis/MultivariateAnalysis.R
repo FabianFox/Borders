b6bc801e060f63e03e3bc1afe604a7484b4fc0af
FabianFox <fabianguelzau@web.de>
2021-01-29 13:48:03 +0100

    mnom regression table

--- analysis/MultivariateAnalysis.R
+++ analysis/MultivariateAnalysis.R
@@ -519,301 +519,6 @@ border.df <- border.df %>%
   mutate(state1_typology_fct = fac_ind_en(state1_typology),
          state1_typology_fct = fct_relevel(state1_typology_fct, "Checkpoint"))
 
-
-#                             LOGISTIC REGRESSION
-### ------------------------------------------------------------------------ ###
-# Apply the glm-formula
-result_glm.df <- model %>%
-  mutate(model = map(formula, ~glm(as.formula(.), 
-                                   family = binomial(link = "logit"), 
-                                   data = border.df)))
-
-# Compute AME (infinitesimal small change)                    
-result_ame.df <- result_glm.df %>%
-  mutate(model = map(model, 
-                     ~margins(.) %>%
-                       summary(.))) %>%
-  # Add p*-stars
-  mutate(model = map(model, ~mutate(., 
-                                    pstars = stars.pval(p))))
-
-# AME of mean -/+ 1 SD                     
-result_sd.df <- result_glm.df %>%
-  mutate(model = map(model, 
-                     ~margins(., change = "sd") %>%
-                       summary(.))) %>%
-  # Add p*-stars
-  mutate(model = map(model, ~mutate(., 
-                                    pstars = stars.pval(p))))
-         
-# Results: Model - bivariate
-### ------------------------------------------------------------------------ ###
-# Filter to bivariate relations
-result_bivariate.df <- result_ame.df %>%
-  filter(!(str_detect(iv, "[+]"))) %>%
-  unnest(model) %>%
-  group_by(dv) %>%
-  nest()
-
-# Create coefplots
-result_bivariate.df <- result_bivariate.df %>%
-  mutate(plots = map2(.x = data, .y = fac_ind_dv(dv), ~ggplot(data = .x) +
-                        geom_point(aes(x = factor, y = AME), stat = "identity") +
-                        geom_errorbar(aes(x = factor, 
-                                          ymin = AME - (SE * qnorm((1-0.95)/2)),
-                                          ymax = AME + (SE * qnorm((1-0.95)/2))
-                                          )) +
-                        geom_hline(yintercept = 0, colour = "gray", linetype = 2) +
-                        ylim(-2, 2) +
-                        coord_flip() +
-                        labs(
-                          title = .y,
-                          x = "", y = "") +
-                        theme_minimal()
-  ))
-
-# Display all coefplots
-bivariate.fig <- wrap_plots(result_bivariate.df$plots)
-
-# Full model
-### ------------------------------------------------------------------------ ###
-# Filter to multivariate model
-result_full.df <- result_ame.df %>%
-  filter(str_detect(iv, "[+]")) %>%
-  unnest(model) %>%
-  group_by(dv) %>%
-  nest()
-
-# Create coefplots
-result_full.df <- result_full.df %>%
-  mutate(plots = map2(.x = data, .y = fac_ind_dv(dv), ~ggplot(data = .x) +
-                        geom_point(aes(x = factor, y = AME), stat = "identity") +
-                        geom_errorbar(aes(x = factor, 
-                                          ymin = AME - (SE * qnorm((1-0.95)/2)),
-                                          ymax = AME + (SE * qnorm((1-0.95)/2))
-                        )) +
-                        geom_hline(yintercept = 0, colour = "gray", linetype = 2) +
-                        ylim(-1, 1) +
-                        coord_flip() +
-                        labs(
-                          title = .y,
-                          x = "", y = "") +
-                        theme_minimal()
-  ))
-
-# Display all coefplots
-multivariate.fig <- wrap_plots(result_full.df$plots)
-
-
-#                           MULTINOMIAL REGRESSION
-### ------------------------------------------------------------------------ ###
-# Notes/issues:
-# - unimputed data
-# Large confidence intervals for...
-# - 'No man's land': variable 'colony' only two cases 
-# - Landmark border: variable 'state1_relig_shrt' no cases in category 'islm'
-
-# Apply multinom
-model_mnom.df <- multinom(
-  as.formula(paste0("state1_typology_fct", " ~ ", iv[12])),
-  Hess = TRUE,
-  data = border.df)
-
-# Tidy
-# Log odds
-result_mnom.df <- model_mnom.df %>%
-  tidy(., conf.int = TRUE, conf.level = 0.95, exponentiate = FALSE) %>% # exponentiate: TRUE
-  filter(term != "(Intercept)") %>%
-  mutate(pstars = stars.pval(p.value),
-         y.level = factor(y.level, 
-                       levels = c("'No man's land'", "Landmark", "Checkpoint", 
-                                  "Barrier", "Fortified")),
-         term = case_when(
-           term == "state1_relig_shrtislm" ~ "relig_muslim",
-           term == "state1_relig_shrtother" ~ "relig_other",
-           TRUE ~ as.character(term)),
-         term_fc = fct_rev(factor(term, 
-                                  levels = c("state1_gdp_log",
-                                             "ratio_gdp",
-                                             "state1_polity",  
-                                             "absdiff_pol", 
-                                             "state1_military_expenditure_perc_gdp",
-                                             "state1_nterror_log",
-                                             "refugees_incoming_log",
-                                             "relig_muslim",
-                                             "relig_other",
-                                             "diff_relig_shrt",
-                                             "colony", 
-                                             "comlang_off"),
-                                  labels = c("GDP pc (log), builder",
-                                             "GDP pc, ratio",
-                                             "Polity, builder",
-                                             "Polity, abs. difference",
-                                             "Military expenditures pc (log), builder",
-                                             "Terror incidents (log), builder",
-                                             "Refugees, incoming (log)",
-                                             "Religion, Muslim\n[Ref.: Christian]",
-                                             "Religion, Other\n[Ref.: Christian]",
-                                             "Same religion",
-                                             "Colonial history",
-                                             "Common language"))))
- 
-# Odds ratios
-result_mnom_rr.df <- model_mnom.df %>%
-  tidy(., conf.int = TRUE, conf.level = 0.95, exponentiate = TRUE) %>% # exponentiate: TRUE
-  filter(term != "(Intercept)") %>%
-  mutate(pstars = stars.pval(p.value),
-         y.level = factor(y.level, 
-                          levels = c("'No man's land'", "Landmark", "Checkpoint", 
-                                     "Barrier", "Fortified")),
-         term = case_when(
-           term == "state1_relig_shrtislm" ~ "relig_muslim",
-           term == "state1_relig_shrtother" ~ "relig_other",
-           TRUE ~ as.character(term)),
-         term_fc = fct_rev(factor(term, 
-                                  levels = c("state1_gdp_log",
-                                             "ratio_gdp",
-                                             "state1_polity",  
-                                             "absdiff_pol", 
-                                             "state1_military_expenditure_perc_gdp",
-                                             "state1_nterror_log",
-                                             "refugees_incoming_log",
-                                             "relig_muslim",
-                                             "relig_other",
-                                             "diff_relig_shrt",
-                                             "colony", 
-                                             "comlang_off"),
-                                  labels = c("GDP pc (log), builder",
-                                             "GDP pc, ratio",
-                                             "Polity, builder",
-                                             "Polity, abs. difference",
-                                             "Military expenditures pc (log), builder",
-                                             "Terror incidents (log), builder",
-                                             "Refugees, incoming (log)",
-                                             "Religion, Muslim\n[Ref.: Christian]",
-                                             "Religion, Other\n[Ref.: Christian]",
-                                             "Same religion",
-                                             "Colonial history",
-                                             "Common language"))))
-
-# AME
-result_mnom_ame.df <- tibble(
-  category = model_mnom.df$lev,
-  model = list(model_mnom.df)) %>%
-  mutate(estimate = map2(.x = model, .y = category, 
-                    ~summary(margins(model = .x, category = .y,)))) %>% # also useful: change = "sd"
-  select(-model) %>%
-  unnest(cols = estimate) %>%
-  select(1:3) %>%
-  mutate(category = factor(category, 
-                          levels = c("'No man's land'", "Landmark", "Checkpoint", 
-                                     "Barrier", "Fortified")),
-         factor = case_when(
-           factor == "state1_relig_shrtislm" ~ "relig_muslim",
-           factor == "state1_relig_shrtother" ~ "relig_other",
-           TRUE ~ as.character(factor)),
-         term_fc = fct_rev(factor(factor, 
-                                  levels = c("state1_gdp_log",
-                                             "ratio_gdp",
-                                             "state1_polity",  
-                                             "absdiff_pol", 
-                                             "state1_military_expenditure_perc_gdp",
-                                             "state1_nterror_log",
-                                             "refugees_incoming_log",
-                                             "relig_muslim",
-                                             "relig_other",
-                                             "diff_relig_shrt",
-                                             "colony", 
-                                             "comlang_off"),
-                                  labels = c("GDP pc (log), builder",
-                                             "GDP pc, ratio",
-                                             "Polity, builder",
-                                             "Polity, abs. difference",
-                                             "Military expenditures pc (log), builder",
-                                             "Terror incidents (log), builder",
-                                             "Refugees, incoming (log)",
-                                             "Religion, Muslim\n[Ref.: Christian]",
-                                             "Religion, Other\n[Ref.: Christian]",
-                                             "Same religion",
-                                             "Colonial history",
-                                             "Common language"))))
-
-# Plots
-# ---------------------------------------------------------------------------- #
-# Plot (log odds)
-ggplot(data = result_mnom.df) +
-  # not significant
-  geom_point(data = function(x){x[!x$pstars %in% c("**", "***"),]},
-             aes(x = term_fc, y = estimate), stat = "identity", alpha = .3) +
-  geom_errorbar(data = function(x){x[!x$pstars %in% c("**", "***"),]}, 
-    aes(x = term_fc, ymin = conf.low, ymax = conf.high), alpha = .3) +
-  # significant
-  geom_point(data = function(x){x[x$pstars %in% c("**", "***"),]},
-             aes(x = term_fc, y = estimate), stat = "identity") +
-  geom_errorbar(data = function(x){x[x$pstars %in% c("**", "***"),]}, 
-                aes(x = term_fc, ymin = conf.low, ymax = conf.high)) +
-  geom_hline(yintercept = 0, colour = "gray", linetype = 2) +
-  facet_wrap(~y.level) +
-  coord_flip() +
-  scale_y_continuous(breaks = seq(-15, 15, 5), 
-                     labels = c("-15", "-10", "-5", "0", "5", "10", "15"),
-                     limits = c(-16, 16)) +
-  labs(
-    title = "",
-    x = "", y = "") +
-  theme_minimal()
-
-# Plot (AME)
-mnom_ame.fig <- ggplot(data = result_mnom_ame.df %>%
-                         filter(category != "Checkpoint")) +
-  # not significant
-  geom_point(aes(x = term_fc, y = AME), stat = "identity", alpha = .5) +
-  geom_hline(yintercept = 0, colour = "gray", linetype = 2) +
-  facet_wrap(~category) +
-  ylim(-0.6, 0.6) +
-  coord_flip() +
-  labs(
-    title = "",
-    x = "", y = "") +
-  theme_minimal()
-
-# ggeffects (see: https://strengejacke.github.io/ggeffects/)
-result_mnom.gg <- ggeffect(model_mnom.df, terms = "state1_gdp_log") %>%
-  mutate(response.level = if_else(response.level == "X.No.man.s.land.", 
-                                  "'No man's land'", response.level),
-         response.level = factor(response.level, 
-                                levels = c("'No man's land'", "Landmark", "Checkpoint", 
-                                           "Barrier", "Fortified")))
-# Plot
-ggplot(result_mnom.gg, aes(x, predicted)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1) +
-  facet_wrap(~response.level) +
-  scale_x_continuous(breaks = log1p(c(400, 1000, 3000, 8000, 20000, 80000)), 
-                     labels = c(400, 1000, 3000, 8000, 20000, 80000)) + 
-  labs(
-    title = "",
-    x = "", y = "") +
-  theme_minimal()
-
-# Interpretation: AME at specific values
-# ---------------------------------------------------------------------------- #
-# AME (+/- 1 SD)
-result_mnom_ame_sd.df <- tibble(
-  category = model_mnom.df$lev,
-  model = list(model_mnom.df)) %>%
-  mutate(estimate = map2(
-    .x = model, .y = category, 
-    ~summary(
-      margins(
-        model = .x, category = .y, 
-        variables = c("state1_gdp_log"), 
-        change = "sd")))) %>%
-  select(-model) %>%
-  unnest(cols = estimate) %>%
-  select(1:3) %>% 
-  arrange(factor)
-
                           ##########################
                           #   MULTIPLE IMPUTATION  #
                           ##########################
@@ -963,6 +668,7 @@ ame_results.df <- ame_results.df %>%
                                               "Common language"))))
 
 # Plot
+# ---------------------------------------------------------------------------- #
 # Create coefplots
 result_mnom_ame.fig <- ame_results.df %>%
   ggplot() +
@@ -989,6 +695,27 @@ result_mnom_ame.fig <- ame_results.df %>%
         axis.text = element_text(size = 10),
         axis.text.x = element_text(angle = 0, hjust = 0.5),
         panel.spacing.x = unit(2, "lines"))
+
+# Regression table (Appendix)
+# ---------------------------------------------------------------------------- #
+ame_results.gt <- ame_results.df %>% 
+  mutate(pstars = ifelse(pstars == ".", "", pstars),
+         coef = paste0(round(coef, 3), pstars),
+         se = paste0("(", round(se, 3), ")")) %>%
+  select(variable, typology, coef, se) %>%
+  pivot_longer(cols = c("coef", "se")) %>%
+  pivot_wider(names_from = typology, values_from = value) %>%
+  select(-name) %>%
+  mutate(Checkpoint = c("Base outcome", rep(NA, 23))) %>%
+  relocate(Checkpoint, .before = `barrier border`) %>%
+  set_names(nm = c("Independent variables", "'No man's land'", "Landmark", "Checkpoint", 
+              "Barrier", "Fortified"))
+
+# create gt-table
+ame_results.gt <- gt(ame_results.gt) %>%
+  fmt_missing(columns = everything(), missing_text = "") %>%
+  cols_align(align = "left", columns = c("Independent variables")) %>%
+  cols_align(align = "right", columns = c(2:6))
 
                           ##########################
                           #   MICE: MULTINOM IN R  #
@@ -1118,15 +845,5 @@ ggsave(
 
 # Appendix
 # Figure A1
-# Independent variables
-ggsave(
-  plot = border_monvars_pwork.fig, "Y:/Grenzen der Welt/Projekte/Walls, barriers, checkpoints and landmarks/Figures/FigA1 - Independent Variables.tiff", width = 14, height = 8, unit = "in",
-  dpi = 300
-)
-
-# Figure A2
-# Logistic regression: A1
-ggsave(
-  plot = bivariate_A1.fig, "Y:/Grenzen der Welt/Projekte/Walls, barriers, checkpoints and landmarks/Figures/A2 - Logistic Regression A1.tiff", width = 14, height = 8, unit = "in",
-  dpi = 300
-)
+# Regression table
+gtsave(data = ame_results.gt, "Y:/Grenzen der Welt/Projekte/Walls, barriers, checkpoints and landmarks/Figures/Tab2 - Multinomial regression.rtf")
